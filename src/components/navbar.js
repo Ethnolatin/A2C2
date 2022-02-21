cfb45c1b4e1423797fd2fd4d966a2b880e7da528
Ethnolatin <duchesne.fred@gmail.com>
2022-02-21 11:13:32 +0100

    set navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,42 +1,49 @@
-import React from "react";
-import { FaBars } from "react-icons/fa";
+import React, { useState } from "react";
 import { NavLink as Link } from "react-router-dom";
 
 const Navbar = () => {
+    const [navListDisplay, setNavListDisplay] = useState(true)
+    const toggleNavList = () => {
+        setNavListDisplay(!navListDisplay);
+    }
+
+const navLinks = [
+    { page: "/", title: "Atelier A²C²"},
+    { page: "/archi", title: "Architecture"},
+    { page: "/aventure", title: "Aventure"},
+    { page: "/couture", title: "Couture"},
+    { page: "/comedie", title: "Comédie"},
+    { page: "/about", title: "A propos"},
+]
+    
   	return (<>
         <nav>
-            <FaBars className="bars"/>
-            <div className="navMenu">
-                <Link className="navLink"
-                  to="/" 
-                >
-                    Atelier A²C²
-                </Link>
-                <Link className="navLink"
-                  to="/archi" 
-                >
-                    Architecture
-                </Link>
-                <Link className="navLink"
-                  to="/aventure" 
-                >
-                    Aventure
-                </Link>
-                <Link className="navLink"
-                  to="/couture" 
-                >
-                    Couture
-                </Link>
-                <Link className="navLink"
-                  to="/comedie" 
-                >
-                    Comédie
-                </Link>
-                <Link className="navLink"
-                  to="/about" 
-                >
-                    A propos
-                </Link>
+            <button
+                type="button"
+                className="hamburger"
+                onClick={() => toggleNavList()}
+            >
+                <div className={`threeBars ${navListDisplay ? "arrow" : ""}`}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </button>
+
+            <div className= {`navMenu ${navListDisplay ? "comesIn" : "comesOut"}`}>
+            {navLinks.map(navLink => {
+                return (
+                    <div key={navLinks.indexOf(navLink)} className="navButton">
+                        <Link
+                            className="navLink"
+                            to={navLink.page} 
+                            onClick={() => toggleNavList()}
+                        >
+                            {navLink.title}
+                        </Link>
+                    </div>
+                )
+            })}
             </div> 
         </nav> 
     </>);
