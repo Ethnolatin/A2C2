c632440c86dc6ed7ece4a2b93f38ca8fbd1a027e
Ethnolatin <duchesne.fred@gmail.com>
2022-03-28 15:52:30 +0200

    archi page last part

--- src/pages/archi.js
+++ src/pages/archi.js
@@ -23,6 +23,7 @@ import lettre_archi9 from '../images/lettre_archi9.jpg';
 import lettre_archi9_thumbnail from '../images/lettre_archi9_thumbnail.jpg';
 import lettre_archi10 from '../images/lettre_archi10.jpg';
 import lettre_archi10_thumbnail from '../images/lettre_archi10_thumbnail.jpg';
+import archi_diplomee from '../images/archi_diplomee.jpg';
 import { NavLink as Link } from "react-router-dom";
 import { SRLWrapper } from "simple-react-lightbox";
 
@@ -50,23 +51,21 @@ const Archi = () => {
                     <strong>Espace de bien-être : CHEZ VOUS</strong>
                 </div>
                 <div>
-                    <div>
-                        <h2>Pratiquer & transmettre autrement :</h2>
-                        <p>Suite à mon expérience d'architecte / maître d’œuvre salariée en agence, je souhaite aujourd'hui <strong>partager</strong> ma boite à outils d'architecte de manière <strong>créative et ludique</strong> pour tous (enfants, adolescents, adultes & famille) au sein de structures variées locales : particuliers, associatifs, scolaires, collectivités et privés ...</p>
-                        <br />
-                        <p><strong>Les objectifs des ateliers d'architecture</strong> sont de :
-                        </p>
-                        <ul>
-                            <li><strong>Prendre conscience</strong> de votre environnement & patrimoine.</li>
-                            <li><strong>Être capable de lire, de comprendre</strong> vos espaces.
-                            </li>
-                            <li><strong>Analyser et répondre</strong> à vos véritables usages.</li>
-                            <li><strong>Devenir acteurs</strong> de vos espaces de vie par l'expérimentation.</li>
-                            <li><strong>Être conscient</strong> des enjeux de l'avenir de notre territoire.</li>
-                            <li><strong>Consommer responsable</strong> en cohérence avec vos projets et valeurs de vie.</li>
-                            <li><strong>Découvrir l'envers du décor</strong> dans l'acte de concevoir & bâtir.</li>
-                        </ul>
-                    </div>
+                    <h2>Pratiquer & transmettre autrement</h2>
+                    <p>Suite à mon expérience d'architecte / maître d’œuvre salariée en agence, je souhaite aujourd'hui <strong>partager</strong> ma boite à outils d'architecte de manière <strong>créative et ludique</strong> pour tous (enfants, adolescents, adultes & famille) au sein de structures variées locales : particuliers, associatifs, scolaires, collectivités et privés ...</p>
+                    <br />
+                    <p><strong>Les objectifs des ateliers d'architecture</strong> sont de :
+                    </p>
+                    <ul>
+                        <li><strong>Prendre conscience</strong> de votre environnement & patrimoine.</li>
+                        <li><strong>Être capable de lire, de comprendre</strong> vos espaces.
+                        </li>
+                        <li><strong>Analyser et répondre</strong> à vos véritables usages.</li>
+                        <li><strong>Devenir acteurs</strong> de vos espaces de vie par l'expérimentation.</li>
+                        <li><strong>Être conscient</strong> des enjeux de l'avenir de notre territoire.</li>
+                        <li><strong>Consommer responsable</strong> en cohérence avec vos projets et valeurs de vie.</li>
+                        <li><strong>Découvrir l'envers du décor</strong> dans l'acte de concevoir & bâtir.</li>
+                    </ul>
                 </div>
                 <div>
                     <SRLWrapper>
@@ -117,6 +116,7 @@ const Archi = () => {
                     <p>Atelier participatif sur vos espaces de vie.<br/>Pour adultes.</p>
                 </div>
             </div>
+            <small><em>Pour les particuliers, le règlement des ateliers se fait le jour de l'atelier à Marie Bourquin directement et uniquement par chèque ou espèces.<br/>Pour les structures ou organisations, le règlement des ateliers peut se faire par virement, à la réception de la facture.</em></small>
             <hr />
             <h2>Je suis archi-passée par ici :</h2>
             <div className="container">
@@ -131,6 +131,23 @@ const Archi = () => {
                         })}
                     </div>
                 </SRLWrapper>
+            </div>
+            <hr/>
+            <h2>Dans l'épisode précédent, je pratiquais l'Architecture de manière conventionnelle</h2>
+            <div className="container">
+                <div className="logos-title">
+                    <img src={archi_diplomee} className="logo" alt="architecte diplômée" />
+                </div>
+                <div className="linkContainer">
+                    <p>Cette première casquette d'Architecte est le socle de ma boîte à outils. Vous retrouverez ce parcours ici&nbsp;:</p>
+                    <Link
+                        className="linkButton"
+                        to="/archi/parcours"
+                        target="_blank"
+                    >
+                        Parcours d'Architecte
+                    </Link>
+                </div>
             </div>
         </main>
     )
